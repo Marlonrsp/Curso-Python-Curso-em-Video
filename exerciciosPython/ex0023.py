2f9c5c4bb24f061ac90b51d818444f19876c9bde
Marlonrsp <57508767+Marlonrsp@users.noreply.github.com>
2022-07-11 13:44:44 -0300

    Create ex0023.py

---
+++ exerciciosPython/ex0023.py
@@ -0,0 +1,11 @@
+#Exercício de separação de números de forma a qual não utiliza sequencia de repetição (if-else)
+num = int(input('Informe um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('Analisando o número {}'.format(num))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
